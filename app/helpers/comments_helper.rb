5bafd04dbbbae80e23c7e2f28904d3956126e182
angel_cadiz <angel.garciafernandez@hotmail.com>
2020-09-25 17:40:36 +0200

    Improved link helpers, improved views, more comment views

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,14 +1,13 @@
 module CommentsHelper
   def comment_links(comment, actions)
     owner = comment && comment.author == current_user
-    member_method = comment && comment.id.present?
     post = comment.post
     published = post.published?
     {
         new: -> { link_to('New comment', new_post_comment_path(post), class: 'btn btn-primary') if published },
-        show: -> { link_to('Show', post_comment_path(post,comment), class: 'btn btn-primary') if member_method },
-        edit: -> { link_to('Edit', edit_post_comment_path(post, comment), class: 'btn btn-primary') if member_method && owner },
-        destroy: -> { link_to('Destroy', post_comment_path(post, comment), method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-primary') if member_method && owner },
+        show: -> { link_to('Show', post_comment_path(post,comment), class: 'btn btn-primary') },
+        edit: -> { link_to('Edit', edit_post_comment_path(post, comment), class: 'btn btn-primary') if owner },
+        destroy: -> { link_to('Destroy', post_comment_path(post, comment), method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-primary') if owner },
         back: -> { link_to('Back', :back, class: 'btn btn-secondary') }
     }.fetch_values(*Array(actions)).map(&:call)
   end
